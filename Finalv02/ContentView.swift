4553dc9e9e086d1cbc0b04ec5e48e02fdf291af8
Elif Dede Yildirim <elifdy@auburn.edu>
2024-04-17 23:00:20 -0400

    Initial Commit

---
+++ Finalv02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finalv02
+//
+//  Created by Elif Dede on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finalv02/Finalv02App.swift
@@ -0,0 +1,17 @@
+//
+//  Finalv02App.swift
+//  Finalv02
+//
+//  Created by Elif Dede on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Finalv02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
